e0db86b6e98cf97d28bb007a56487a833d88c932
Ofer Hopstone Groman <oferg2@brainpop.com>
2025-02-02 12:26:23 +0200

    Add item scroll support

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="todo" class="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
+  <div id="todo" class="bg-white shadow-md rounded-lg p-6 w-full max-w-md h-3/4 flex flex-col">
     <div class="flex flex-col gap-2 relative mb-4">
       <h2 class="text-2xl font-bold text-gray-700">To-Do List</h2>
       <div class="relative w-full">
@@ -7,17 +7,15 @@
         <button class="absolute top-0 end-0 bg-orange-500 text-white px-8 py-2 rounded-full" @click="addItemClicked()">Add</button>
       </div>
     </div>
-    <div class="space-y-4">
+    <div class="flex-1 overflow-auto space-y-4">
       <div class="flex flex-col gap-2 px-2">
-          <Item
-              v-for="item in items"
-              :key="item.id"
-              :item="item"
-              @remove-item="(key) => removeItem(key)"
-          />
+        <Item
+            v-for="item in items"
+            :key="item.id"
+            :item="item"
+            @remove-item="(key) => removeItem(key)"
+        />
       </div>
-
-      <!-- Add more todo items here -->
     </div>
   </div>
 </template>
